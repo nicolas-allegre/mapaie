6d4b0312de66611d9ee9d72934b57a367f4e3f9a
simondelarue <sim.delarue@gmail.com>
2023-12-18 14:07:39 +0100

    added preprocessing script

---
+++ preprocess.py
@@ -0,0 +1,49 @@
+import glob
+from tqdm import tqdm
+import numpy as np
+
+import nltk as nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+corpus = []
+stop_words = set(stopwords.words('english'))
+
+print(f'Preprocessing...')
+for i, filename in enumerate(tqdm(glob.glob('txts/*.txt'))):
+    name = filename.split('/')[1].split('.')[0]
+    with open(filename) as f:
+        lines = f.read().strip()
+        # Tokenize
+        tokens = word_tokenize(lines)
+        # Remove tokens with length < 3
+        tokens = (' ').join([t.lower() for t in tokens if len(t) > 3 and t.isalpha() and t.lower() not in stop_words])
+        # Save tokens
+        corpus.append(tokens)
+
+
+# TF-IDF
+def tfidf_filter(corpus):
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(corpus)
+    tfidf_values = np.array(X.mean(axis=0))[0]
+    median_tfidf = np.quantile(tfidf_values, 0.5)
+    mask = tfidf_values > median_tfidf
+    words_to_keep = vectorizer.get_feature_names_out()[mask]
+    return words_to_keep
+
+corpus_filt_tfidf = []
+words_to_keep = tfidf_filter(corpus)
+
+for d in tqdm(corpus):
+    words = d.split()
+    filt_words = [w for w in words if w in words_to_keep]
+    corpus_filt_tfidf.append(filt_words)
+
+with open('corpus_filt_tfidf.txt', 'w') as f:
+    for d in corpus_filt_tfidf:
+        doc = ' '.join(d)
+        f.write(doc + '\n')
