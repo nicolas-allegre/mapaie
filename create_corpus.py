fee72e27989cf9e7c8db29aa70557163cecbb414
Tiphaine Viard <tiphaine.viard@telecom-paris.fr>
2024-01-02 21:24:43 +0100

    Create subcorpora per theme

--- create_corpus.py
+++ create_corpus.py
@@ -2,6 +2,7 @@ import sys
 import glob
 from pathlib import Path
 from nltk.corpus import stopwords
+from tqdm import tqdm
 
 LOG_FILE = "corpus.log"
 OUT_FILE = "corpus.txt"
@@ -9,13 +10,18 @@ log_fp = open(LOG_FILE, "w")
 
 corpus_file = open(OUT_FILE, "w", encoding="utf-8")
 
-for fname in glob.glob("./txts/*.txt"):
-    print(fname)
+
+for fname in tqdm(glob.glob(f"./{sys.argv[1]}/*.txt")):
+    topics = ["*mapaie"]
+
     try:
         f = open(fname, "r")
+        contents = f.read().strip()
+        if "fairness" in contents:
+            topics.append("*fairness")
 
-        print("**** *mapaie", file=corpus_file)
-        print(f.read().strip(), file=corpus_file)
+        print("**** " + " ".join(topics), file=corpus_file)
+        print(contents, file=corpus_file)
         
         f.close()
     except Exception as e:
