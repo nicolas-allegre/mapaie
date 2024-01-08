c9f548c3332e1dd75f554501be3ac6de09299d5a
Tiphaine Viard <tiphaine.viard@telecom-paris.fr>
2024-01-08 16:50:06 +0100

    Added dowloading for html files

--- dl_docs.py
+++ dl_docs.py
@@ -9,8 +9,8 @@ requests.packages.urllib3.disable_warnings()
 
 URL_FILE = "list_urls.txt"
 UA_FILE = "user_agents.txt"
-OUT_FOLDER = "./pdfs"
-LOG_FILE = "dl_pdfs.log"
+OUT_FOLDER = "./docs"
+LOG_FILE = "dl_docs.log"
 log_fp = open(LOG_FILE, "w")
 
 list_of_urls = [ x.strip() for x in open(URL_FILE).readlines() ]
@@ -30,7 +30,7 @@ for i in tqdm(range(len(list_of_urls))):
 
     if response.status_code == 200:
         print(f"{url},OK", file=log_fp)
-        if ".pdf"[-4:] == ".pdf":
+        if url[-4:] == ".pdf":
             with open(f"{OUT_FOLDER}/{i}.pdf", "wb") as f:
                 f.write(response.content)
         else:
