fc9bc6f7dc7d8739b8d327c62f95e6cb0db280fb
Tiphaine Viard <tiphaine.viard@telecom-paris.fr>
2024-01-19 18:20:51 +0100

    Themes

--- create_corpus.py
+++ create_corpus.py
@@ -1,3 +1,4 @@
+import numpy
 import sys
 import glob
 from pathlib import Path
@@ -14,14 +15,16 @@ nb_docs = 0
 
 # Keywords
 keywords = {
-    "fairness": ["fair", "fairness"],
+    "fairness": [" fairness ", " algorithmic fairness ", " demographic parity "],
     "accountability": ["accountability", "accountable"],
-    "xai": ["xai", "explainability", "explainable"],
-    "transparency": ["transparency", "transparent"]
+    "xai": [" xai ", " lime ", " shap "],
+    "transparency": ["transparency", "transparent"],
+    "agi": [" agi " ],
+    "dignity": [" dignity "],
+    "gen_ai": [" generative "]
 }
 # keywords = json.load(open("themes.json"))
 
-filt_crit = lambda x, kw_list: (len(set(x))) >= 3 or (len(kw_list) == 1 and len(set(x)) == 1)
 filt_crit = lambda x, kw_list: all(x)
 
 # Counting docs per theme
@@ -41,10 +44,6 @@ for i, fname in enumerate(tqdm(glob.glob(f"./{sys.argv[1]}/*.txt"))):
             if filt_crit([ kw for kw in kw_list if kw in contents ], kw_list):
             # if any([ kw in contents for kw in kw_list ]):
                 doc_occurrences[i][topic] = sum([contents.count(x) for x in kw_list])
-
-                # topics.append(f"*{topic}")
-                doc_counts[topic] += 1
-
         
         f.close()
     except Exception as e:
@@ -53,12 +52,11 @@ for i, fname in enumerate(tqdm(glob.glob(f"./{sys.argv[1]}/*.txt"))):
 
 # Write out topics
 topics_counts = { x: [] for x in keywords.keys() }
-topics_medians = { x: [] for x in keywords.keys() }
+topics_medians = { x: 0.0 for x in keywords.keys() }
 for i in doc_occurrences:
     for t in keywords:
-        topics_medians[t].append(doc_occurrences[i][t])
+        topics_counts[t].append(doc_occurrences[i][t])
 
-import numpy
 for t in topics_medians:
     topics_medians[t] = numpy.median(topics_counts[t])
 
@@ -67,8 +65,14 @@ for i in doc_occurrences:
     topics = ["*mapaie"]
 
     for t in keywords:
-        if doc_occurrences[i][t] > topics_medians[t]:
-            topics.append(f"*{topic}")
+        # if doc_occurrences[i][t] > topics_medians[t]:
+        filt_crit = [ x in doc_occurrences[i]["contents"] for x in keywords[t] ] 
+        # doc_has_topic = any(filt_crit)
+        doc_has_topic = len([x for x in filt_crit if x])/len(filt_crit) >= 0.6
+
+        if doc_has_topic:
+            topics.append(f"*{t}")
+            doc_counts[t] += 1
 
     print("**** " + " ".join(topics), file=corpus_file)
     print(doc_occurrences[i]["contents"], file=corpus_file)
@@ -76,9 +80,8 @@ for i in doc_occurrences:
 log_fp.close()
 corpus_file.close()
 
-# Decide topics now
-
 # regarder aussi les co-occurrences de thèmes
 print("Summary stats")
 for k, v in doc_counts.items():
     print(f"{k}: {v} ({v/nb_docs*100}%)") 
+
