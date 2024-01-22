3356899e2aa8cf90ce02e5337eb7f5ee164d4453
Tiphaine Viard <tiphaine.viard@telecom-paris.fr>
2024-01-22 12:04:21 +0100

    Added argument parsing and help to create corpus

--- create_corpus.py
+++ create_corpus.py
@@ -1,3 +1,4 @@
+import argparse
 import numpy
 import sys
 import glob
@@ -5,33 +6,52 @@ from pathlib import Path
 from nltk.corpus import stopwords
 from tqdm import tqdm
 import ujson as json
+import os
 
 LOG_FILE = "corpus.log"
 OUT_FILE = "corpus.txt"
 log_fp = open(LOG_FILE, "w")
 
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--data")
+parser.add_argument("-m", "--method", choices=["iramuteq", "cortext"])
+parser.add_argument("-t", "--themes")
+# parser.add_argument("-d", "--destination", choices=["iramuteq", "cortext"])
+
+args = parser.parse_args()
+print(args)
+
 corpus_file = open(OUT_FILE, "w", encoding="utf-8")
 nb_docs = 0
 
 # Keywords
-keywords = {
-    "fairness": [" fairness ", " algorithmic fairness ", " demographic parity "],
-    "accountability": ["accountability", "accountable"],
-    "xai": [" xai ", " lime ", " shap "],
-    "transparency": ["transparency", "transparent"],
-    "agi": [" agi " ],
-    "dignity": [" dignity "],
-    "gen_ai": [" generative "]
-}
-# keywords = json.load(open("themes.json"))
+keywords = json.load(open(args.themes))
 
 filt_crit = lambda x, kw_list: all(x)
+
+iramuteq = False
+cortext = False
+
+
+if args.method == "iramuteq":
+    iramuteq = True
+    cortext = False
+elif args.method == "cortext":
+    cortext = True
+    iramuteq = False
+
+    for t in keywords:
+        if not os.path.exists(t):
+            os.makedirs(t)
+else:
+    iramuteq = True
 
 # Counting docs per theme
 doc_counts = { k: 0 for k, v in keywords.items() }
 doc_occurrences = {}
 
-for i, fname in enumerate(tqdm(glob.glob(f"./{sys.argv[1]}/*.txt"))):
+for i, fname in enumerate(tqdm(glob.glob(f"./{args.data}/*.txt"))):
     nb_docs += 1
     doc_occurrences[i] = {}
 
@@ -73,9 +93,19 @@ for i in doc_occurrences:
         if doc_has_topic:
             topics.append(f"*{t}")
             doc_counts[t] += 1
+    
+    # Corpus Iramuteq
+    if iramuteq:
+        print("**** " + " ".join(topics), file=corpus_file)
+        print(doc_occurrences[i]["contents"], file=corpus_file)
 
-    print("**** " + " ".join(topics), file=corpus_file)
-    print(doc_occurrences[i]["contents"], file=corpus_file)
+    # Corpus cortext
+    if cortext:
+        for t in topics:
+            # Creer dir topics
+            if t.strip("*") != "mapaie":
+                file = open(f"{t.strip('*')}/{i}.txt", "w")
+                print(doc_occurrences[i]["contents"], file=file) 
 
 log_fp.close()
 corpus_file.close()
